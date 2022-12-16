e87b172971c9f1ac5da8100b9c0b4a8a98d8eb77
Serj112 <raskolnikov.112@mail.ru>
2022-12-16 21:56:11 +0300

    Add project files.

---
+++ Skillfactory_10_FinalTask/Program.cs
@@ -0,0 +1,72 @@
+﻿using Skillfactory_10_FinalTask;
+
+public interface ILogger
+{
+    void Event(string message);
+    void Error(string message);
+}
+
+public class Logger: ILogger
+{
+    public void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+    }
+
+    public void Event(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+    }
+}
+
+public interface IWorker
+{
+    void Work();
+}
+
+public interface ICalculator
+{
+    int Sum(int a, int b);
+}
+
+public class Calculator: ICalculator
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+public class Program
+{
+    static ILogger Logger { get; set; }
+    static void Main()
+    {
+        Logger = new Logger();
+        var worker1 = new Worker1(Logger);
+
+        worker1.Work();
+
+        try
+        {
+            var calculator = new Calculator();
+
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Сумма чисел равна: " + ((ICalculator)calculator).Sum(a, b));
+        }
+
+        catch
+        {
+            worker1.Error();
+        }
+        
+    }
+}
---
+++ Skillfactory_10_FinalTask/Worker1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillfactory_10_FinalTask
+{
+    public class Worker1: IWorker
+    {
+        ILogger Logger { get;}
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Logger.Event("Калькулятор начал свою работу");      
+        }
+
+        public void Error()
+        {
+            Logger.Error("В работе калькулятора допущена ошибка");
+        }
+    }
+}
